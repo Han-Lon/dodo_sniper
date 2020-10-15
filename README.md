0c2198ff771bab7a35b0c8ee43d41a890e7ff1ae
Discount Surgeon <JMor0707@gmail.com>
2020-10-15 15:30:20 -0500

    Initial commit. Transferring cleaned, sanitized code from private repository to public

---
+++ README.md
@@ -0,0 +1,10 @@
+## Dodo Sniper
+### A CLI tool for grabbing that sick item on https://dodocodes.com/
+
+## Requires Firefox and the Firefox 'geckodriver' at https://github.com/mozilla/geckodriver/releases
+How to use:
+- Clone down from GitHub
+- To install Python prerequisite packages, execute `pip install -r requirements.txt` in the directory
+- Make sure the geckodriver.exe file is also in this directory (same directory as main.py)
+- Run the program by executing `python main.py <URL to the auction>` in this directory
+- Voila! Firefox will automatically open and prompt you to login at Dodocodes
---
+++ main.py
@@ -0,0 +1,96 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+import argparse
+import getpass
+import os
+from datetime import datetime, timedelta
+from time import sleep
+
+# Need to know if the auction is in tickets or bells to set the ceiling and find quick bid button
+while True:
+    xpath_bid_btn = str(input("Tickets or Bells?: "))
+    if xpath_bid_btn.lower() == 'tickets':
+        # Don't go higher than 10 Nook mile tickets
+        ceiling = 10
+        xpath_bid_btn = '//*[@id="btn-quick-bid-1"]'
+        break
+    elif xpath_bid_btn.lower() == 'bells':
+        # Don't go higher than 150000 bells
+        ceiling = 280000
+        xpath_bid_btn = '//*[@id="btn-quick-bid-1000"]'
+        break
+    else:
+        print('Error. The input needs to be either bells or ticets. Was {}'.format(xpath_bid_btn))
+
+# Snipe when there's this many seconds left
+seconds = 2
+
+
+def login(driver):
+    """Login to dodocodes using username+password. Requires minimal user interaction"""
+    print('Logging in')
+    driver.find_element_by_xpath('/html/body/header/nav/div/ul[2]/li/a').click()
+    username_field = driver.find_element_by_xpath('//*[@id="input-email"]')
+    passw_field = driver.find_element_by_xpath('//*[@id="input-password"]')
+    username_field.send_keys(os.environ['username'])
+    passw_field.send_keys(os.environ['password'])
+    passw_field.send_keys(Keys.RETURN)
+    print('Done logging in')
+
+
+def discord_login(driver):
+    """Login to dodocodes using Discord. Requires user interaction"""
+    print('Logging in')
+    driver.find_element_by_xpath('/html/body/header/nav/div/ul[2]/li/a').click()
+    driver.find_element_by_xpath('//*[@id="at-discord"]').click()
+    input('Enter anything here and press return when you\'ve successfully logged in to Discord: ')
+    print('Done logging in')
+
+
+def snipe(driver):
+    """Find time left in auction, wait until 2 seconds before auction ends (while checking ceiling), check
+       ceiling one more time, and then snipe"""
+    time = driver.find_element_by_xpath('/html/body/main/div/div[5]/div/div[1]/div[1]/div[3]/div[2]/h4').text.split()[-1]
+    time = datetime.strptime(time, "%H:%M:%S").time()
+    tnow = timedelta(hours=time.hour, minutes=time.minute, seconds=time.second)
+    tsnipe = timedelta(hours=0, minutes=0, seconds=seconds)
+    twait = tnow - tsnipe
+    print('Waiting for... {}'.format(str(twait)))
+    current_bid = driver.find_element_by_xpath('/html/body/main/div/div[5]/div/div[1]/div/div[3]/div[1]/div[2]/h4').text
+    try:
+        # Don't bid if the current bid is higher than the ceiling (upper limit)
+        if current_bid < ceiling:
+            pass
+        else:
+            print('Current bid is {}. Too high, not bidding'.format(str(current_bid)))
+            exit(69)
+    except TypeError as e:
+        print('No bids expected: {}'.format(current_bid))
+    # Sleep for a few seconds
+    sleep(twait.total_seconds())
+    print('Wait is over')
+    current_bid = driver.find_element_by_xpath('/html/body/main/div/div[5]/div/div[1]/div/div[3]/div[1]/div[2]/h4').text
+    try:
+        # If the current bid is lower than the ceiling, place a bid. Otherwise, exit the program
+        if current_bid < ceiling:
+            driver.find_element_by_xpath(xpath_bid_btn).click()
+        else:
+            print('Current bid is {}. Too high, not bidding'.format(str(current_bid)))
+            exit(1)
+    except TypeError as e:
+        print('No bids expected: {}'.format(current_bid))
+        driver.find_element_by_xpath(xpath_bid_btn).click()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('url')
+    args = parser.parse_args()
+    driver = webdriver.Firefox()
+    driver.get(args.url)
+    driver.implicitly_wait(10)
+    discord_login(driver)
+    snipe(driver)
+
+    
